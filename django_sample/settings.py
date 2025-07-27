7e3e3d4ebee0b7a86a24695b81c1e6e0904d2b07
Mark <markolofsen@gmail.com>
2025-07-27 20:46:25 +0800

    Update Django settings: Enhance security and adjust project configurations.

--- django_sample/settings.py
+++ django_sample/settings.py
@@ -273,6 +273,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    'https://revolution.unrealos.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
